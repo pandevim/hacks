1b53a983568fe7bf0c6361bb14fbcb104d372191
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-01-23 03:12:05 +0530

    Create db_setup.py

---
+++ wed-jan23-19/google_form_spammer_v03/db_setup.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+connect = sqlite3.connect('dummy_data.db')
+c = connect.cursor()
+
+try:
+	c.execute("""CREATE TABLE DUMMY_DATA (
+		name text,
+		reg_no integer,
+		roll_no integer,
+		mob_no integer
+	)""")
+except sqlite3.OperationalError:
+	None
+
+# c.execute("""CREATE TABLE IF NOT EXIST DUMMY_DATA (
+# 	name text,
+# 	reg_no integer,
+# 	roll_no integer,
+# 	mob_no integer
+# )""")
+
+# c.execute("INSERT INTO DUMMY_DATA VALUES ('Amir Khan', 11756893, 43, 9657894847)")
+c.execute("SELECT NAME FROM DUMMY_DATA")
+c.fetchall()
+
+connect.commit()
+connect.close()
