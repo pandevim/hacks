c295c04e8a57b8749af4c8fc7024eed4dc833fca
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-01-24 12:29:20 +0530

    Update db_setup.py

--- wed-jan23-19/google_form_spammer_v03/db_setup.py
+++ wed-jan23-19/google_form_spammer_v03/db_setup.py
@@ -1,17 +1,25 @@
+from faker import Faker
+from faker.providers import phone_number
 import sqlite3
+import random
+import string
+
+fake = Faker()
+fake.add_provider(phone_number)
 
 connect = sqlite3.connect('dummy_data.db')
 c = connect.cursor()
 
 try:
-	c.execute("""CREATE TABLE DUMMY_DATA (
+	c.execute("""
+	CREATE TABLE DUMMY_DATA (
 		name text,
 		reg_no integer,
 		roll_no integer,
 		mob_no integer
 	)""")
 except sqlite3.OperationalError:
-	None
+	pass
 
 # c.execute("""CREATE TABLE IF NOT EXIST DUMMY_DATA (
 # 	name text,
@@ -20,9 +28,17 @@ except sqlite3.OperationalError:
 # 	mob_no integer
 # )""")
 
-c.execute("INSERT INTO DUMMY_DATA VALUES ('Amir Khan', 11756893, 43, 9657894847)")
+for _ in range(0, 10):
+	c.execute("INSERT INTO DUMMY_DATA VALUES (?,?,?,?)" , (
+		fake.name(),
+		str(random.randint(11700000, 11799999)),
+		str(random.randint(1, 68)),
+		fake.phone_number()
+	))
+
 c.execute("SELECT * FROM DUMMY_DATA")
-c.fetchall()
+
+
 
 connect.commit()
 connect.close()
