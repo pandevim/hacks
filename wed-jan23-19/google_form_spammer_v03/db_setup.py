88fba5f35b6445a177cea33af7c8d61dc266e0ab
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-01-24 18:14:00 +0530

    Update db_setup.py

--- wed-jan23-19/google_form_spammer_v03/db_setup.py
+++ wed-jan23-19/google_form_spammer_v03/db_setup.py
@@ -4,15 +4,14 @@ import sqlite3
 import random
 import string
 
-fake = Faker()
+fake = Faker('hi_IN')
 fake.add_provider(phone_number)
 
 connect = sqlite3.connect('dummy_data.db')
 c = connect.cursor()
 
 try:
-	c.execute("""
-	CREATE TABLE DUMMY_DATA (
+	c.execute("""CREATE TABLE DUMMY_DATA (
 		name text,
 		reg_no integer,
 		roll_no integer,
@@ -30,14 +29,14 @@ except sqlite3.OperationalError:
 
 for _ in range(0, 10):
 	c.execute("INSERT INTO DUMMY_DATA VALUES (?,?,?,?)" , (
-		fake.name(),
+		fake.first_name() + ' ' + fake.last_name(),
 		str(random.randint(11700000, 11799999)),
 		str(random.randint(1, 68)),
 		fake.phone_number()
 	))
 
 c.execute("SELECT * FROM DUMMY_DATA")
-
+input_data = c.fetchall()
 
 
 connect.commit()
