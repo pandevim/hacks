80d8a7c92eb02fbb3446b843756abe32df83d788
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-03-19 18:04:07 +0530

    Update packages.py

--- packages.py
+++ packages.py
@@ -1,10 +1,14 @@
 # this will give the available package versions
 # pip install package==
 
-def packages():
+def packages_1():
   if pip.__version__ >= "10.0.0":
       from pip._internal.utils.misc import get_installed_distributions
       sorted(["%s==%s" % (i.key, i.version) for i in get_installed_distributions()])
   else:
       from pip import get_installed_distributions
       sorted(["%s==%s" % (i.key, i.version) for i in get_installed_distributions()])
+
+def packages_2():
+  from pkg_resources import working_set
+  sorted(["%s==%s" % (d.project_name, d.version) for d in working_set])
