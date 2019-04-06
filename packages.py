83190494536a7d829a00df8d9e449eff5965305a
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-04-06 21:17:34 +0530

    Update packages.py

--- packages.py
+++ packages.py
@@ -4,10 +4,9 @@
 def packages_1():
   if pip.__version__ >= "10.0.0":
       from pip._internal.utils.misc import get_installed_distributions
-      sorted(["%s==%s" % (i.key, i.version) for i in get_installed_distributions()])
   else:
       from pip import get_installed_distributions
-      sorted(["%s==%s" % (i.key, i.version) for i in get_installed_distributions()])
+  sorted(["%s==%s" % (i.key, i.version) for i in get_installed_distributions()])
 
 def packages_2():
   from pkg_resources import working_set
